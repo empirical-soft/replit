4371aa033c0858093fda5fc063d93615cd1ec384
Christopher C. Aycock <christopher.aycock@empirical-soft.com>
2020-08-31 23:15:03 -0400

    Bumped to 0.6.6

--- setup_repl.sh
+++ setup_repl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.6.5
+VERSION=0.6.6
 if [[ ! -f empirical ]]; then
   wget https://github.com/empirical-soft/empirical-lang/releases/download/$VERSION/linux.zip
   unzip linux.zip
